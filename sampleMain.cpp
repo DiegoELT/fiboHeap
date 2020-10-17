48053c723691bdc3b75ff008c8ae37a34f330035
Piero Angelo Morales Alcalde <38793411+Piero16301@users.noreply.github.com>
2020-10-17 17:55:12 -0500

    Se agrega la lectura de archivos

--- sampleMain.cpp
+++ sampleMain.cpp
@@ -1,123 +1,198 @@
-#include <iostream>
+/*#include <iostream>
 #include <list>
 #include <vector>
-#include <string>
-#include <fstream>
-#include <sstream>
 #include <cmath>
 
 template <typename T>
 class NodoFibonacci{
-  private:
+private:
     T m_dato{};
     std::list<NodoFibonacci<T> *> m_hijos;
-  public:
+public:
     NodoFibonacci(T m_dato) : m_dato(m_dato) {};
     T obtenerDato(){
-      return this -> m_dato;
+        return this -> m_dato;
     }
     T obtenerRango(){
-      return this -> m_hijos.size();
+        return this -> m_hijos.size();
     }
     std::list<NodoFibonacci<T> *> obtenerHijos(){
-      return this-> m_hijos;
+        return this-> m_hijos;
     }
     void giveChild(NodoFibonacci<T> * child){
-      m_hijos.push_back(child);
+        m_hijos.push_back(child);
     }
 };
 
 template <typename T>
 class HeapFibonacci{
-  private:
+private:
     int size = 0;
     std::list<NodoFibonacci<T> *> m_heap;
     typename std::list<NodoFibonacci<T>*>::iterator m_pMin = m_heap.end();
-    
-  public:
-    typename std::list<NodoFibonacci<T>*>::iterator unir(typename std::list<NodoFibonacci<T>*>::iterator nodeA, typename std::list<NodoFibonacci<T>*>::iterator nodeB){ 
-      if ((*nodeA)->obtenerDato() <= (*nodeB)->obtenerDato()) {
-        m_heap.erase(nodeB);
-        (*nodeA)->giveChild(*nodeB);
-        return nodeA;
-      } else {
-        m_heap.erase(nodeA);
-        (*nodeB)->giveChild(*nodeA);
-        return nodeB;
-      }
-    }
-    
-    void compactar(){
-      std::vector<typename std::list<NodoFibonacci<T>*>::iterator> gradeCounter(log2(size)+1, m_heap.end());
-      auto it = m_heap.end();
-      it--;
-      for (; it != m_heap.begin(); it--) {
-        if (gradeCounter[(*it)->obtenerRango()] != m_heap.end() && gradeCounter[(*it)->obtenerRango()] != it) {
-          int n = (*it)->obtenerRango();
-          // std::cout << "El grado del elemento: " << (*it)->obtenerDato() << " y " << (*gradeCounter[(*it)->obtenerRango()])->obtenerDato() << " son iguales \n";
-          it = unir(it, gradeCounter[(*it)->obtenerRango()]);
-          // std::cout << "El iterador se encuentra en: " << (*it)->obtenerDato() << std::endl;
-          it++;
-          gradeCounter[n] = m_heap.end();
-        } else{
-          gradeCounter[(*it)->obtenerRango()] = it;
+
+public:
+    typename std::list<NodoFibonacci<T>*>::iterator unir(typename std::list<NodoFibonacci<T>*>::iterator nodeA, typename std::list<NodoFibonacci<T>*>::iterator nodeB){
+        if ((*nodeA)->obtenerDato() <= (*nodeB)->obtenerDato()) {
+            m_heap.erase(nodeB);
+            (*nodeA)->giveChild(*nodeB);
+            return nodeA;
+        } else {
+            m_heap.erase(nodeA);
+            (*nodeB)->giveChild(*nodeA);
+            return nodeB;
         }
-      }
-      while (gradeCounter[(*it)->obtenerRango()] != m_heap.end()) {
-          it = unir(it, gradeCounter[(*it)->obtenerRango()]);
-      }
     }
-    
+
+    void compactar(){
+        std::vector<typename std::list<NodoFibonacci<T>*>::iterator> gradeCounter(log2(size)+1, m_heap.end());
+        auto it = m_heap.end();
+        it--;
+        for (; it != m_heap.begin(); it--) {
+            if (gradeCounter[(*it)->obtenerRango()] != m_heap.end() && gradeCounter[(*it)->obtenerRango()] != it) {
+                int n = (*it)->obtenerRango();
+                // std::cout << "El grado del elemento: " << (*it)->obtenerDato() << " y " << (*gradeCounter[(*it)->obtenerRango()])->obtenerDato() << " son iguales \n";
+                it = unir(it, gradeCounter[(*it)->obtenerRango()]);
+                // std::cout << "El iterador se encuentra en: " << (*it)->obtenerDato() << std::endl;
+                it++;
+                gradeCounter[n] = m_heap.end();
+            } else{
+                gradeCounter[(*it)->obtenerRango()] = it;
+            }
+        }
+        while (gradeCounter[(*it)->obtenerRango()] != m_heap.end()) {
+            it = unir(it, gradeCounter[(*it)->obtenerRango()]);
+        }
+    }
+
     void extractMin(){
-      for (auto hijo : (*m_pMin)->obtenerHijos()) {
-        m_heap.push_back(hijo);
-      }
-      m_heap.erase(m_pMin);
-      size--;
-      // printTree();
-      compactar();
-      m_pMin = m_heap.end();
-      for (auto it = m_heap.begin(); it != m_heap.end(); it++){
-        if(m_pMin == m_heap.end() || (*m_pMin) -> obtenerDato() > (*it) -> obtenerDato())
-          m_pMin = it;
-      }
+        for (auto hijo : (*m_pMin)->obtenerHijos()) {
+            m_heap.push_back(hijo);
+        }
+        m_heap.erase(m_pMin);
+        size--;
+        // printTree();
+        compactar();
+        m_pMin = m_heap.end();
+        for (auto it = m_heap.begin(); it != m_heap.end(); it++){
+            if(m_pMin == m_heap.end() || (*m_pMin) -> obtenerDato() > (*it) -> obtenerDato())
+                m_pMin = it;
+        }
     }
-    
+
     typename std::list<NodoFibonacci<T>*>::iterator getMin(){
-      return m_pMin;
+        return m_pMin;
     }
-    
+
     void insert(T dato){
-      NodoFibonacci<T> * nuevoNodo = new NodoFibonacci(dato);
-      this -> m_heap.push_back(nuevoNodo);
-      if(m_pMin == m_heap.end() || (*m_pMin) -> obtenerDato() > nuevoNodo -> obtenerDato())
-      {
-        m_pMin = m_heap.end(); // Como es el ultimo insert, es el ultimo valor de la lista
-        m_pMin--;
-      }
-      size++;
+        NodoFibonacci<T> * nuevoNodo = new NodoFibonacci(dato);
+        this -> m_heap.push_back(nuevoNodo);
+        if(m_pMin == m_heap.end() || (*m_pMin) -> obtenerDato() > nuevoNodo -> obtenerDato())
+        {
+            m_pMin = m_heap.end(); // Como es el ultimo insert, es el ultimo valor de la lista
+            m_pMin--;
+        }
+        size++;
     }
-    
+
     void printTree(){
-      for(auto elem : m_heap)
-        std::cout << elem -> obtenerDato() << "," << elem -> obtenerRango() << " -> ";
-      std::cout << "\n";
+        for(auto elem : m_heap)
+            std::cout << elem -> obtenerDato() << "," << elem -> obtenerRango() << " -> ";
+        std::cout << "\n";
     }
-    
+
     int getSize(){
-      return size;
+        return size;
     }
 };
 
 int main() {
-  HeapFibonacci<int> testFib;
-  for(int i = 10000; i >= 1; i--){
-    testFib.insert(i);
-  }
-  for(int i = 10000; i >= 2; i--){
+    HeapFibonacci<int> testFib;
+    for(int i = 10000; i >= 1; i--){
+        testFib.insert(i);
+    }
+    for(int i = 10000; i >= 2; i--){
+        std::cout << (*testFib.getMin())->obtenerDato() << std::endl;
+        testFib.extractMin();
+    }
     std::cout << (*testFib.getMin())->obtenerDato() << std::endl;
-    testFib.extractMin();
-  }
-  std::cout << (*testFib.getMin())->obtenerDato() << std::endl;
-  return 0;
+    return 0;
+}*/
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "CImg.h"
+
+using namespace cimg_library;
+using namespace std;
+
+struct imagen {
+    string ruta;
+    vector <float> vc;
+};
+
+struct arista {
+    string origen;
+    string destino;
+    float peso{};
+};
+
+vector <float> vectorizar(CImg <float> &img) {
+    vector <float> R;
+    cimg_forXY(img, x, y) {
+        R.emplace_back((img(x,y,0) + img(x,y,1) + img(x,y,2)) / 3);
+    }
+    return R;
+}
+
+int main() {
+
+    system("ls ../imagenes/ \"*.jpg\" > ../database.txt");
+
+    vector <arista> aristas;
+
+    vector <imagen> imagenes;
+
+    ifstream rutas("../database.txt");
+    string ruta;
+    getline(rutas, ruta);
+    while (getline(rutas, ruta)) {
+        //cout << "../imagenes/" << ruta << endl;
+        string temp = "../imagenes/" + ruta;
+        CImg <float> A(temp.c_str());
+        A.resize(128,128);
+        //cout << A._width << A._height << endl;
+        CImg <float> B = A.haar(false, 2);
+        CImg <float> C = B.crop(0,0,27,27);
+
+        vector <float> vc = vectorizar(C);
+
+        imagen img;
+        img.ruta = temp;
+        img.vc = vc;
+
+        imagenes.emplace_back(img);
+
+        //A.display();
+        //B.display();
+        //C.display();
+    }
+
+    for (int i = 0; i < imagenes.size()-1; i++) {
+        for (int j = i+1; j < imagenes.size(); j++) {
+            arista ar;
+            ar.origen = imagenes[i].ruta;
+            ar.destino = imagenes[j].ruta;
+            float suma = 0;
+            for (int k = 0; k < imagenes[i].vc.size(); k++) {
+                suma += pow(imagenes[i].vc[k] - imagenes[j].vc[k], 2);
+            }
+            ar.peso = sqrt(suma);
+            aristas.emplace_back(ar);
+        }
+    }
+
+    return 0;
 }
