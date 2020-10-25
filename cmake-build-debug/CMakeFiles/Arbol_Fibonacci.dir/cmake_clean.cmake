b43abbf1a0244c57086a4efc7f43c1596d10421d
Piero Morales <piero.morales@utec.edu.pe>
2020-10-24 21:43:13 -0500

    Update

---
+++ cmake-build-debug/CMakeFiles/Arbol_Fibonacci.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Arbol_Fibonacci"
+  "Arbol_Fibonacci.pdb"
+  "CMakeFiles/Arbol_Fibonacci.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Arbol_Fibonacci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
